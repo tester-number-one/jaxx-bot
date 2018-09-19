f1a751167151b74dfa5c37a5ac8976a8c5fb9709
Blackjaxx9 <38385929+Blackjaxx9@users.noreply.github.com>
2018-09-19 19:45:02 +0200

    reformatting to d.js

--- app.js
+++ app.js
@@ -1,34 +1,21 @@
-const Eris = require("eris");
-var bot = new Eris.CommandClient(process.env.REG_BOT_TOKEN, {}, {
-    description: "My bot refactored from d.js to Eris",
-    owner: "SerialCereal",
-    prefix: "."
-});
+const Discord = require('discord.js');
+var bot = new Discord.Client();
 const configs = ('./config.json');
 const responses = require('./responses.json');
 bot.on("ready", () => { 
   console.log('I am ready! :D'); 
 }); 
-bot.on("messageCreate", (msg) => { 
+bot.on("message", (message) => { 
   const fruit_response = responses.fruit[Math.floor(Math.random() * responses.fruit.length)];
   const args = msg.content.slice((process.env.PREFIX).length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-  if (!(msg.author.id === process.env.MASTER_ID)) return; //checks messages only with my id
-  if (msg.channel.id === process.env.EVAL_TEST) bot.createMessage(process.env.EVAL_TEST,eval(msg.content));
- 
+  if (!(message.author.id === process.env.MASTER_ID)) return; //checks messages only with my id 
   switch(command)
   {
-    case "pingme":
-      bot.createMessage(msg.channel.id,"BOOPERS");
-      break;
     case "boop":
-      bot.createMessage(msg.channel.id,fruit_response);
-      break;
-    case "evalme":
-      var doit = args.slice(0).join(" ");
-      bot.createMessage(msg.channel.id,eval(doit)+" ");
+      message.channel.send(fruit_response);
       break;
   }
 });
-bot.connect();
+bot.login(process.env.BOT_TOKEN);
 //documentation is at https://discord.js.org/#/docs/main/stable/general/welcome ;)
